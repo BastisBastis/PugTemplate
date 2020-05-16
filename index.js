66ff7c62a9cf03c739d74f75dc1074daa2ecf497
BastisBastis <65193082+BastisBastis@users.noreply.github.com>
2020-05-16 13:19:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const people = require('./people.json');
 
 const app = express();
 
-
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index', {
