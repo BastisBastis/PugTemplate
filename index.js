d5a929881184b65cd2fa148704b841e0cbe18c47
BastisBastis <65193082+BastisBastis@users.noreply.github.com>
2020-05-16 13:10:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
+
 
 app.set('view engine', 'pug');
 
-// serve static files from the `public` folder
-app.use(express.static(__dirname + '/public'));
-
-
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Homepage'
+  res.render('index' {
+    title: 'Homepage',
+    people: people.profiles
   });
 });
 
